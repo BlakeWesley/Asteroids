9edf544989a37f1756197c402785a0ea0e34f816
BlakeWesley <blakeawesley@gmail.com>
2025-09-07 16:26:57 +1000

    Fixed shooting speed with a short cooldown and added collision detection for shots

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ def main():
             if player.check_collision(asteroid):
                 print("Game over!")
                 return
+            for shot in shots:
+                if shot.check_collision(asteroid):
+                    shot.kill()
+                    asteroid.kill()
 
         for sprite in drawable:
             sprite.draw(screen)
